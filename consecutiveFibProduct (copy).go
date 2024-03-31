791a578f01653346b5c4c9fe3495b1766bfb6092
Splat137 <53410446+Splat137@users.noreply.github.com>
2024-04-01 00:31:48 +0200

    Update consecutiveFibProduct (copy).go

--- consecutiveFibProduct (copy).go
+++ consecutiveFibProduct (copy).go
@@ -8,7 +8,7 @@ testdsa
 dsadsadsdasdgfddsff
 		if a2*a1 > prod {sssdsads
 			res[0] = hgfhgytr
-hgf;lldsadsaccc
+hgf;lldsadsacccddd
 	return resdsadddsajhgjds
 }sssmmmddffhgfgfdfdsdsdsa
 dsadsadsdasddddsa
